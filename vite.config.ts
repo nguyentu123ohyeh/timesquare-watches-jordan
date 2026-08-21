e991c03e56ec29d477795dcca0d475055b742d54
minhtv901 <le5875970@gmail.com>
2026-08-21 18:47:09 +0700

    done build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-
   base: "/timesquare-watches-jordan/",
-
-  tanstackStart: {
-    prerender: {
-      enabled: true,
-    },
-  },
-
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
 });
